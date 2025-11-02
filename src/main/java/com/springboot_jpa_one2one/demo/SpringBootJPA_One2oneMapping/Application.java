fa74fdba94d553d5ad4b8f5465df92f0ca700646
Chakali_Krishna <manyamkrishna925@gmail.com>
2025-11-02 23:06:32 +0530

    Initial commit

---
+++ src/main/java/com/springboot_jpa_one2one/demo/SpringBootJPA_One2oneMapping/Application.java
@@ -0,0 +1,13 @@
+package com.springboot_jpa_one2one.demo.SpringBootJPA_One2oneMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_jpa_one2one/demo/SpringBootJPA_One2oneMapping/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_jpa_one2one.demo.SpringBootJPA_One2oneMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
